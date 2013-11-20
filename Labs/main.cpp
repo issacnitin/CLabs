bd6d14f6c9ad802f70e88d57e06ef5bb70de9a86
Nitin Issac Joy <nitin.i.joy@gmail.com>
2013-11-21 01:35:45 +0530

    init

--- Labs/main.cpp
+++ Labs/main.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
+#include <yeahbabe.h>
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    main2();
     return 0;
+
 }
 
---
+++ Labs/yeahbabe.h
@@ -0,0 +1,11 @@
+#ifndef YEAHBABE_H
+#define YEAHBABE_H
+
+#endif // YEAHBABE_H
+
+#include<iostream>
+using namespace std;
+
+void main2(){
+    cout<<"You basically need to \n write functions as header files and rename them to .cpp afterwards \n for judges to check :D\n";
+}
